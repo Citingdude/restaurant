6407a474c8c6921b5aa4b564f73d8eedbfb1e9da
Glenn <glenn.reumers@appwise.be>
2024-07-03 18:15:55 +0200

    Init project with gulp

---
+++ gulpfile.ts
@@ -0,0 +1,32 @@
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import concat from 'gulp-concat'
+
+const sass = gulpSass(dartSass)
+
+// function buildStyles() {
+//   return gulp.src('./scss/base/_button.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./public/css'));
+// };
+
+// function buildStyles() {
+//   return gulp.src('./scss/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./public/css'));
+// };
+
+function buildStyles() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/css'));
+};
+
+function defaultTask(cb: () => void) {
+  buildStyles()
+  cb();
+}
+
+exports.default = defaultTask;
