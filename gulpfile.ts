48859825f6e8670e771917189ee44701d14ecb0c
citingdude <glenn.reumers@appwise.be>
2024-07-06 12:07:47 +0200

    Config http server

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,18 +5,6 @@ import concat from 'gulp-concat'
 
 const sass = gulpSass(dartSass)
 
-// function buildStyles() {
-//   return gulp.src('./scss/base/_button.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./public/css'));
-// };
-
-// function buildStyles() {
-//   return gulp.src('./scss/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./public/css'));
-// };
-
 function buildStyles() {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
